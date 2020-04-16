017e6c4937cf94ba2a8b3e691b3738578ff46aee
TChi91 <adm4algnw@gmail.com>
2020-04-16 15:02:33 +0100

    put djangoVue in a separate function

--- cmd/new.go
+++ cmd/new.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"io"
 	"log"
 	"os"
 	"os/exec"
@@ -9,33 +8,21 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var djangoVue string = "https://github.com/yaseralnajjar/django-vue-template"
-
-func copyAndCapture(w io.Writer, r io.Reader) ([]byte, error) {
-	var out []byte
-	buf := make([]byte, 1024, 1024)
-	for {
-		n, err := r.Read(buf[:])
-		if n > 0 {
-			d := buf[:n]
-			out = append(out, d...)
-			_, err := w.Write(d)
-			if err != nil {
-				return out, err
-			}
-		}
-		if err != nil {
-			// Read returns io.EOF at the end of file, which is not an error for us
-			if err == io.EOF {
-				err = nil
-			}
-			return out, err
-		}
-	}
-}
-
 func init() {
 	rootCmd.AddCommand(newCmd)
+}
+
+func djangoVue() {
+	var repo string = "https://github.com/yaseralnajjar/django-vue-template"
+	command := exec.Command("git", "clone", repo)
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+
+	err := command.Run()
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+
 }
 
 var newCmd = &cobra.Command{
@@ -48,15 +35,7 @@ var newCmd = &cobra.Command{
 			log.Fatal(err)
 		}
 
-		command := exec.Command("git", "clone", djangoVue)
-		//command := exec.Command("date")
-		command.Stdout = os.Stdout
-		command.Stderr = os.Stderr
-
-		err = command.Run()
-		if err != nil {
-			log.Fatal("Error:", err)
-		}
+		djangoVue()
 
 	},
 }
