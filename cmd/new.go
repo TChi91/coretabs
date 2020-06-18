3fc9cb468c19551b0fa25b903cca2bc4b34e9bbc
Fethi TChi <adm4algnw@gmail.com>
2020-06-19 00:49:15 +0100

    coretabs cli v0.0.1

--- cmd/new.go
+++ cmd/new.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"errors"
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
@@ -8,14 +10,36 @@ import (
 	"github.com/spf13/cobra"
 )
 
+const (
+	repo string = "https://github.com/TChi91/rest-vue"
+)
+
+var (
+	pathsNeeded = []string{"git", "pip3", "yarn"}
+)
+
 func init() {
 	rootCmd.AddCommand(newCmd)
 }
 
-func djangoVue() {
-	repo := "https://github.com/yaseralnajjar/django-vue-template"
+func checkPaths() (map[string]error, error) {
+	missing := make(map[string]error)
+	for _, path := range pathsNeeded {
+		_, err := exec.LookPath(path)
+		if err != nil {
+			missing[path] = err
+		}
+	}
+	if len(missing) != 0 {
+		return missing, errors.New("missing dependeties")
+	}
+	return nil, nil
 
-	command := exec.Command("git", "clone", repo)
+}
+
+func cloneProject(projectName string) error {
+	fmt.Println("cloning project started ....")
+	command := exec.Command("git", "clone", repo, projectName)
 
 	command.Stdout = os.Stdout
 	command.Stderr = os.Stderr
@@ -24,18 +48,35 @@ func djangoVue() {
 	if err != nil {
 		log.Fatal("Error:", err)
 	}
+	return nil
 
 }
 
-func setProjectName(args []string) {
-	if len(args) != 2 {
-		log.Fatal("new command can be only with two args")
-	}
+func changeDirectory(dir string) error {
+	err := os.Chdir(dir)
 
-	err := os.Rename("django-vue-template", args[1])
 	if err != nil {
 		log.Fatal("Error:", err)
 	}
+	return nil
+}
+
+func installRequirments() error {
+	fmt.Println("starting virtual env ....")
+	command := exec.Command("bash", "-c",
+		"yarn install; yarn build; pip3 install virtualenv; virtualenv venv; source venv/bin/activate; pip3 install -r requirements.txt; python3 manage.py migrate")
+
+	// command := exec.Command("pip3", "install", "requirements.txt")
+
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+
+	err := command.Run()
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+	return nil
+
 }
 
 var newCmd = &cobra.Command{
@@ -43,22 +84,35 @@ var newCmd = &cobra.Command{
 	Short: "create new project",
 	Long:  `with new you can create new project`,
 	Run: func(cmd *cobra.Command, args []string) {
-		_, err := exec.LookPath("git")
+		missing, err := checkPaths()
+		if err != nil {
+			for key, value := range missing {
+				fmt.Println(key, ":", value)
+			}
+			fmt.Println("")
+			return
+		}
+		var projectName string
+		// var projectType string
+		fmt.Print("Project Name: ")
+		if _, err := fmt.Scanf("%s", &projectName); err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+		// fmt.Print("Project Type: ")
+
+		// if _, err := fmt.Scanf("%s", &projectType); err != nil {
+		// 	fmt.Printf("%s\n", err)
+		// 	return
+		// }
+
+		err = cloneProject(projectName)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		switch args[0] {
-		case "fullstack":
-			switch len(args) {
-			case 1:
-				djangoVue()
-			case 2:
-				djangoVue()
-				setProjectName(args)
-			}
-
-		}
+		err = changeDirectory(projectName)
+		err = installRequirments()
 
 	},
 }
