096b150dd271924b79c68eb26bda8884fba00ff4
Fethi TChi <adm4algnw@gmail.com>
2020-07-13 01:56:29 +0100

    set default servers ports

--- config/app.go
+++ config/app.go
@@ -5,3 +5,12 @@ type AppConfig struct {
 	FrontEnd FrontEndConfiguration `yaml:"frontend"`
 	BackEnd  BackEndConfiguration  `yaml:"backend"`
 }
+
+// NewConfig Creates new instance of AppConfig
+func NewConfig() AppConfig {
+	config := AppConfig{}
+	config.BackEnd.Port = 8000
+	config.FrontEnd.Port = 8080
+
+	return config
+}
